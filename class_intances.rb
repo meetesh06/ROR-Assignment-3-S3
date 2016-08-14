7748d2f13b67ed8ff9c4c582804c6d1670fb2b8f
meetesh06 <meeteshmehta4@gmail.com>
2016-08-14 19:42:57 +0530

    init repo

---
+++ class_intances.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/ruby -w
+print "Enter first number : "
+
+a = gets.to_i
+
+print "Enter second number : "
+b = gets.to_i
+class Jello
+	def sum(a,b)
+		a+b
+	end
+	def self.s_sum(a,b)
+		a+b
+	end
+end
+
+obj = Jello.new
+print "Using Object Method : "
+puts obj.sum(a,b)
+print "Calling Class Method : "
+puts Jello.s_sum(a,b)
